0ebc053ea48fb2ffa749557fa0a8a35ede341a20
Oscar N <PaintNinja@users.noreply.github.com>
2021-01-18 16:23:30 +0000

    Backport 1.0.5 from 1.16 to 1.14

--- src/main/java/dk/zlepper/itlt/Itlt.java
+++ src/main/java/dk/zlepper/itlt/Itlt.java
@@ -11,13 +11,16 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ServerData;
 import net.minecraft.client.multiplayer.ServerList;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.lwjgl.glfw.GLFW;
@@ -40,6 +43,7 @@ public final class Itlt {
         System.setProperty("java.awt.headless", "false");
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientInit);
 
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.CLIENT_CONFIG);
         Config.loadConfig(Config.CLIENT_CONFIG, FMLPaths.CONFIGDIR.get().resolve("itlt-client.toml"));
 
--- src/main/java/dk/zlepper/itlt/about/mod.java
+++ src/main/java/dk/zlepper/itlt/about/mod.java
@@ -4,5 +4,5 @@ public final class mod {
     public static final String
             ID = "itlt",
             NAME = "It's the little things",
-            VERSION = "1.0.3";
+            VERSION = "1.0.5";
 }

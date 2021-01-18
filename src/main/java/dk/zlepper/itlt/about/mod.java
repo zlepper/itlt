a4cf07a970238ad810277bf0911970e932b59ca8
Oscar N <PaintNinja@users.noreply.github.com>
2021-01-18 15:23:30 +0000

    Backport 1.0.5 from 1.16, update mappings & Forge
    Now requires Forge 31.2.47 or newer

--- src/main/java/dk/zlepper/itlt/about/mod.java
+++ src/main/java/dk/zlepper/itlt/about/mod.java
@@ -4,5 +4,5 @@ public final class mod {
     public static final String
             ID = "itlt",
             NAME = "It's the little things",
-            VERSION = "1.0.4";
+            VERSION = "1.0.5";
 }

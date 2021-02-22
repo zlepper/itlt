6cb8108ea109f35e1b05eefc0a9df4d25fa5cbd2
Oscar N <PaintNinja@users.noreply.github.com>
2021-02-22 21:37:10 +0000

    Backport 1.0.1 to MC 1.11.x, fixes #11
    Also properly setup this branch so that it's easy to get going right away after cloning

--- src/main/java/dk/zlepper/itlt/about/mod.java
+++ src/main/java/dk/zlepper/itlt/about/mod.java
@@ -3,5 +3,5 @@ package dk.zlepper.itlt.about;
 public final class mod {
     public static final String ID = "itlt";
     public static final String NAME = "It's the little things";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 }

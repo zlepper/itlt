a16447ecf20de5226e11eb00afecf99580589e60
Oscar N <PaintNinja@users.noreply.github.com>
2021-02-25 21:46:46 +0000

    Finish backport to 1.5.x

--- src/main/java/dk/zlepper/itlt/proxies/CommonProxy.java
+++
@@ -1,7 +0,0 @@
-package dk.zlepper.itlt.proxies;
-
-/**
- * Created by Rasmus on 7/16/2015.
- */
-public class CommonProxy  {
-}
--- src/main/java/dk/zlepper/itlt/proxies/ServerProxy.java
+++
@@ -1,7 +0,0 @@
-package dk.zlepper.itlt.proxies;
-
-/**
- * Created by Rasmus on 7/20/2015.
- */
-public class ServerProxy extends CommonProxy {
-}

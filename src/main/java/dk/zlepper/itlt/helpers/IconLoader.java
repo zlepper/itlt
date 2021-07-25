78608b58fa0fc087329190313ecbcbfa9dda3ca5
Kaydax <kaydax@kaydax.xyz>
2021-07-25 03:29:31 -0400

    Add hacked together support for ico and icns

--- src/main/java/dk/zlepper/itlt/helpers/IconLoader.java
+++ src/main/java/dk/zlepper/itlt/helpers/IconLoader.java
@@ -18,9 +18,10 @@ import java.nio.ByteBuffer;
  * ratio is kept the same as the source image.
  *
  * @author Chris Molini
+ * @author Paint_Ninja
  *****************************************************************************/
-public class IconLoader
-{
+public class IconLoader {
+
     /*************************************************************************
      * Loads an icon in ByteBuffer form.
      *
@@ -30,46 +31,36 @@ public class IconLoader
      * @return An array of ByteBuffers containing the pixel data for the icon in
      *         varying sizes.
      *************************************************************************/
-    public static ByteBuffer[] load(String filepath)
-    {
+    public static ByteBuffer[] load(final String filepath) {
         return load(new File(filepath));
     }
 
     /*************************************************************************
      * Loads an icon in ByteBuffer form.
      *
-     * @param fil
+     * @param file
      *            A File pointing to the image.
      *
      * @return An array of ByteBuffers containing the pixel data for the icon in
      *         various sizes (as recommended by the OS).
      *************************************************************************/
-    public static ByteBuffer[] load(File fil)
-    {
+    public static ByteBuffer[] load(final File file) {
         BufferedImage image = null;
-        try
-        {
-            image = ImageIO.read(fil);
-        }
-        catch (IOException e)
-        {
+        try {
+            image = ImageIO.read(file);
+        } catch (final IOException e) {
             e.printStackTrace();
         }
-        ByteBuffer[] buffers = null;
-        final String OS = System.getProperty("os.name").toUpperCase();
-        if(OS.contains("WIN"))
-        {
+
+        final ByteBuffer[] buffers;
+        if (Platform.isWindows()) {
             buffers = new ByteBuffer[2];
             buffers[0] = loadInstance(image, 16);
             buffers[1] = loadInstance(image, 32);
-        }
-        else if(OS.contains("MAC"))
-        {
+        } else if (Platform.isMac()) {
             buffers = new ByteBuffer[1];
             buffers[0] = loadInstance(image, 128);
-        }
-        else
-        {
+        } else {
             buffers = new ByteBuffer[1];
             buffers[0] = loadInstance(image, 32);
         }
@@ -86,14 +77,12 @@ public class IconLoader
      *
      * @return A ByteBuffer of pixel data at the indicated size.
      *************************************************************************/
-    private static ByteBuffer loadInstance(BufferedImage image, int dimension)
-    {
-        BufferedImage scaledIcon = new BufferedImage(dimension, dimension,
-                BufferedImage.TYPE_INT_ARGB_PRE);
-        Graphics2D g = scaledIcon.createGraphics();
-        double ratio = getIconRatio(image, scaledIcon);
-        double width = image.getWidth() * ratio;
-        double height = image.getHeight() * ratio;
+    private static ByteBuffer loadInstance(final BufferedImage image, final int dimension) {
+        final BufferedImage scaledIcon = new BufferedImage(dimension, dimension, BufferedImage.TYPE_INT_ARGB_PRE);
+        final Graphics2D g = scaledIcon.createGraphics();
+        final double ratio = getIconRatio(image, scaledIcon);
+        final double width = image.getWidth() * ratio;
+        final double height = image.getHeight() * ratio;
         g.drawImage(image, (int) ((scaledIcon.getWidth() - width) / 2),
                 (int) ((scaledIcon.getHeight() - height) / 2), (int) (width),
                 (int) (height), null);
@@ -114,24 +103,17 @@ public class IconLoader
      * @return The amount to scale the source image to fit it onto the icon
      *         appropriately.
      *************************************************************************/
-    private static double getIconRatio(BufferedImage src, BufferedImage icon)
-    {
-        double ratio = 1;
-        if (src.getWidth() > icon.getWidth())
-            ratio = (double) (icon.getWidth()) / src.getWidth();
-        else
-            ratio = (int) (icon.getWidth() / src.getWidth());
-        if (src.getHeight() > icon.getHeight())
-        {
-            double r2 = (double) (icon.getHeight()) / src.getHeight();
-            if (r2 < ratio)
-                ratio = r2;
-        }
-        else
-        {
-            double r2 = (int) (icon.getHeight() / src.getHeight());
-            if (r2 < ratio)
-                ratio = r2;
+    private static double getIconRatio(final BufferedImage src, final BufferedImage icon) {
+        double ratio;
+        if (src.getWidth() > icon.getWidth()) ratio = (double) (icon.getWidth()) / src.getWidth();
+        else ratio = (int) (icon.getWidth() / src.getWidth());
+
+        if (src.getHeight() > icon.getHeight()) {
+            final double r2 = (double) (icon.getHeight()) / src.getHeight();
+            if (r2 < ratio) ratio = r2;
+        } else {
+            final double r2 = (int) (icon.getHeight() / src.getHeight());
+            if (r2 < ratio) ratio = r2;
         }
         return ratio;
     }
@@ -144,20 +126,19 @@ public class IconLoader
      *
      * @return A ByteBuffer that contains the pixel data of the supplied image.
      *************************************************************************/
-    public static ByteBuffer convertToByteBuffer(BufferedImage image)
-    {
-        byte[] buffer = new byte[image.getWidth() * image.getHeight() * 4];
+    public static ByteBuffer convertToByteBuffer(final BufferedImage image) {
+        final byte[] buffer = new byte[image.getWidth() * image.getHeight() * 4];
         int counter = 0;
-        for (int i = 0; i < image.getHeight(); i++)
-            for (int j = 0; j < image.getWidth(); j++)
-            {
+        for (int i = 0; i < image.getHeight(); i++) {
+            for (int j = 0; j < image.getWidth(); j++) {
                 int colorSpace = image.getRGB(j, i);
-                buffer[counter + 0] = (byte) ((colorSpace << 8) >> 24);
+                buffer[counter    ] = (byte) ((colorSpace << 8) >> 24);
                 buffer[counter + 1] = (byte) ((colorSpace << 16) >> 24);
                 buffer[counter + 2] = (byte) ((colorSpace << 24) >> 24);
                 buffer[counter + 3] = (byte) (colorSpace >> 24);
                 counter += 4;
             }
+        }
         return ByteBuffer.wrap(buffer);
     }
 }

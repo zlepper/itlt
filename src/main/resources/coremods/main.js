ce99dbbb38445158f2b2a57d9819d79171325b37
Oscar N <PaintNinja@users.noreply.github.com>
2021-08-23 23:00:06 +0100

    Apply lib workaround suggested on Forgecord, fix coremod

--- src/main/resources/coremods/main.js
+++ src/main/resources/coremods/main.js
@@ -2,17 +2,15 @@ function initializeCoreMod() {
     var opcodes = Java.type("org.objectweb.asm.Opcodes");
     var InsnNode = Java.type("org.objectweb.asm.tree.InsnNode");
 
-    /**
-     * @author Paint_Ninja
-     * @reason Restores the ability to change the Minecraft window title
-     * Todo: Make a Forge PR for a "WindowTitleChangedEvent" or similar
-     */
+    // Author: Paint_Ninja
+    // Reason: Restores the ability to change the Minecraft window title
+    // Todo: Make a Forge PR for a "WindowTitleChangedEvent" or similar
     return {
         "CustomWindowTitle": {
             "target": {
                 "type": "METHOD",
                 "class": "net.minecraft.client.Minecraft",
-                "methodName": "func_230150_b_",
+                "methodName": "updateTitle",
                 "methodDesc": "()V"
             },
             "transformer": function(methodNode) {

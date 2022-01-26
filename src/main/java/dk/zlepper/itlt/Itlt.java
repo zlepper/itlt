71bd29c4e22c566d345d7a7bd6a0775eca28efc4
Paint_Ninja <PaintNinja@users.noreply.github.com>
2022-01-26 13:58:11 +0000

    Backport 1.0.2 to MC 1.8.8/9

--- src/main/java/dk/zlepper/itlt/Itlt.java
+++ src/main/java/dk/zlepper/itlt/Itlt.java
@@ -27,7 +27,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
 
-@Mod(modid = mod.ID, version = mod.VERSION, name = mod.NAME)
+@Mod(modid = mod.ID, version = mod.VERSION, name = mod.NAME, acceptableRemoteVersions = "*")
 public class Itlt {
     @Mod.Instance("itlt")
     public static Itlt instance;
